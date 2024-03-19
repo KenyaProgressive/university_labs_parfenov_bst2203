9d2b85f24045e0ae28ba152461f910ae325c08a0
KenyaProgressive <ya.nikita-parfenov@yandex.ru>
2024-03-19 20:40:56 +0300

    complete lab-2

--- lab-2/StackDack.py
+++ lab-2/StackDack.py
@@ -1,88 +1,53 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-        self.prev = None
-
-
 class Stack:
     def __init__(self):
-        self.top = None
+        self.items = []
 
-    def is_empty(self):
-        return self.top is None
+    def push(self, item):
+        self.items.append(item)
 
-    def push(self, data):
-        new_node = Node(data)
-        new_node.next = self.top
-        self.top = new_node
-
-    def cut(self):
-        if self.is_empty():
-            return None
-        popped = self.top
-        self.top = self.top.next
-        return popped.data
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
 
     def peek(self):
-        if self.is_empty():
-            return None
-        return self.top.data
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def show(self):
+        print(self.items)
 
 
 class Deque:
     def __init__(self):
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.items = []
+
+    def add_front(self, item):
+        self.items.insert(0, item)
+
+    def add_rear(self, item):
+        self.items.append(item)
+
+    def peek_front(self):
+        if not self.is_empty():
+            return self.items[0]
+
+    def peek_rear(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def pop_front(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+
+    def pop_rear(self):
+        if not self.is_empty():
+            return self.items.pop()
 
     def is_empty(self):
-        return self.head.next == self.tail
+        return not self.items
 
-    def push_top(self, data):
-        head_node = Node(data)
-        head_node.prev = self.head
-        head_node.next = self.head.next
-        self.head.next.prev = head_node
-        self.head.next = head_node
-
-    def push_bottom(self, data):
-        tail_node = Node(data)
-        tail_node.prev = self.tail
-        tail_node.next = self.tail.next
-        self.tail.prev.next = tail_node
-        self.tail.prev = tail_node
-
-    def pop_top(self):
-        if self.is_empty():
-            return None
-        new_node = self.head.next
-        self.head.next = new_node.next
-        new_node.next.prev = self.head
-        self.head = new_node.next
-        return new_node.data
-
-    def pop_bottom(self):
-        if self.is_empty():
-            return None
-        new_node = self.tail.prev
-        self.tail.prev = new_node.prev
-        new_node.prev.next = self.tail
-        return new_node.data
-
-    def peek_top(self):
-        if self.is_empty():
-            return None
-        return self.head.next.data
-
-    def peek_bottom(self):
-        if self.is_empty():
-            return None
-        return self.tail.prev.data
-
-    def show_deque(self):
-        current = self.head
-        while current is not None:
-            print(current.data)
-            current = current.next
+    def size(self):
+        return len(self.items)
--- lab-2/lab-2-ParfenovN.py
+++ lab-2/lab-2-ParfenovN.py
@@ -2,60 +2,222 @@
 # _______________________________________________________________________#
 
 from StackDack import Deque
-
-# from StackDack import Stack
-
-# from StackDack import Stack
+from StackDack import Stack
+import string
 
 # Задание 1
 
 # Отсортировать строки файла, содержащие названия книг, в алфавитном порядке с использованием двух деков.
 
+# deque_1 = Deque()
+# deque_1_1 = Deque()
+#
+# with open("myfile.txt", 'r', encoding='utf-8') as file:
+#     lines = file.readlines()
+#     for line in lines:
+#         deque_1.add_rear(line)
+#
+#     while not deque_1.is_empty():
+#         element = deque_1.pop_rear()
+#         while not deque_1_1.is_empty() and deque_1_1.peek_rear() > element:
+#             deque_1.add_front(deque_1_1.pop_rear())
+#         deque_1_1.add_rear(element)
+#
+# with open("myfile.txt", 'w', encoding='utf-8') as file:
+#     while not deque_1_1.is_empty():
+#         element_ = deque_1_1.pop_front()
+#         file.write(element_)
 
 # -----------------------------------------------------------------------
 
 # Создадим два дека
-# deque_1 = Deque()
-# deque_2 = Deque()
-#
-# with open("myfile.txt", 'r', encoding='utf-8') as file:
-#     strs = file.readlines()
-# for line in strs:
-#     deque_1.push_bottom(line)  # добавим все элементы файла в первый дэк
-#
-#
-# while not (deque_1.is_empty()):
-#     elem = deque_1.pop_top()
-#     while not deque_2.is_empty() and deque_2.pop_bottom() < elem:
-#         deque_1.push_top(deque_2.pop_bottom())
-#     deque_2.push_bottom(elem)
-#
-#
-# deque_1.show_deque()
+
 # -------------------------------------------------------------------------
 
-# Задание 7
+# Задание 2
 
+# Дек содержит последовательность символов для шифровки сообщений.
+# Дан текстовый файл, содержащий зашифрованное сообщение.
+# Пользуясь деком, расшифровать текст.
+# Известно, что при шифровке
+# каждый символ сообщения заменялся следующим за
+# ним в деке по часовой стрелке через один.
+
+deque_2 = Deque()
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for char in alphabet:
+    deque_2.add_rear(char)  # добавим алфавит в дек
+res = ""
+
+with open("file2.txt", 'r',
+          encoding="utf-8") as file:
+    mes = file.read()
+    for char in mes:
+        while char != deque_2.peek_front():  # пока символ сообщения не совпадет с символом алфавита
+            # переносим символы из начала в конец.
+            deque_2.add_rear(deque_2.pop_front())
+        deque_2.add_front(deque_2.pop_rear())
+        res += deque_2.peek_front()
+# print(res)
+
+with open("file2.txt", 'w', encoding="utf-8") as file:
+    file.write(res)
+# -------------------------------------------------------------------------
+# Задание 3
+
+
+# Даны три стержня и n дисков различного размера.
+# Диски можно надевать на стержни, образуя из них башни.
+# Перенести n дисков со стержня А на стержень С, сохранив их первоначальный порядок.
+# При переносе дисков необходимо соблюдать следующие правила:
+# - на каждом шаге со стержня на стержень переносить только один диск;
+# - диск нельзя помещать на диск меньшего размера;
+# - для промежуточного хранения можно использовать стержень В.
+# Реализовать алгоритм, используя три стека вместо стержней А, В, С.
+# Информация о дисках хранится в исходном файле.
+
+first = Stack()  # Башня 1
+second = Stack()  # Башня 2
+third = Stack()  # Башня 3
+helper = 0  # для конвертации информации о дисках в числа
+
+
+# def hanoi_tower_task(n, from_tower, help_tower, to_tower):
+#     if n == 1:
+#         elem = from_tower.pop()
+#         to_tower.push(elem)
+#         print(f"Перемещаем {elem}")
+#     else:
+#         hanoi_tower_task(n - 1, from_tower, help_tower, to_tower)
+#         elem = from_tower.pop()
+#         to_tower.push(elem)
+#         print(f"Перемещаем {elem}")
+#
+# with open("file3.txt", 'r', encoding="utf-8") as file:
+#     disks = file.readlines()
+#
+# for i in range(len(disks)):
+#     helper = int(disks[i])
+#     disks[i] = helper
+#
+# for i in disks:
+#     first.push(i)
+#
+# hanoi_tower_task(len(disks), first, second, third)
+#
+# first.show()
+# second.show()
+# third.show()
+
+# ---------------------------------------------------------------------------
+
+# Задание 4
+
+# Дан текстовый файл с
+# программой на алгоритмическом языке.
+# За один просмотр файла проверить баланс
+# круглых скобок в тексте, используя стек.
+
+# stack_4 = Stack()
+# skobka_counter = 0
+# with open("file4.txt", 'r', encoding="utf-8") as file:
+#     strs = file.readlines()
+#     for line in strs:
+#         for i in range(len(line)):
+#             stack_4.push(line[i])
+#             if stack_4.peek() == "(":
+#                 skobka_counter += 1
+#             elif stack_4.peek() == ")":
+#                 skobka_counter -= 1
+#     if skobka_counter == 0:
+#         print("Баланс скобок сохраняется")
+#     else:
+#         print("Баланс скобок нарушен")
+
+# -------------------------------------------------------------------------
+
+# Задание 5
+# Дан текстовый файл с программой на алгоритмическом языке.
+# За один просмотр файла проверить
+# баланс квадратных скобок в тексте, используя дек.
+
+# deque_5 = Deque()
+# deque_skobka_counter = 0
+#
+# with open("file5.txt", 'r', encoding="utf-8") as file:
+#     strs_ = file.readlines()
+#     for line in strs_:
+#         for i in range(len(line)):
+#             if line[i] == "[":
+#                 deque_5.add_front(line[i])
+#                 deque_skobka_counter += 1
+#             elif line[i] == "]":
+#                 deque_5.add_rear(line[i])
+#                 deque_skobka_counter -= 1
+#     if deque_skobka_counter == 0:
+#         print('Баланс скобок сохраняется')
+#     else:
+#         print('Баланс скобок нарушен')
+
+# ------------------------------------------------------------------------
+
+# Задание 6
+# Дан файл из символов.
+# Используя стек, за один просмотр файла напечатать сначала все цифры,
+# затем все буквы, и, наконец, все остальные символы,
+# сохраняя исходный порядок в каждой группе символов.
+
+# stack_6 = Stack()
+# with open("file6.txt", 'r+', encoding="utf-8") as file:
+#     strs__ = file.readlines()
+#     strs__ = strs__[::-1] # перевернём, чтобы при внесении сохранить изначальный порядок файла
+#     for line in strs__: # запишем в конец символы
+#         for i in range(len(line)):
+#             if not line[i].isdigit() and not line[i].isalpha() and line[i] != "\n":
+#                 stack_6.push(line[i])
+#     for line in strs__: # в середину буквы
+#         for i in range(len(line)):
+#             if line[i].isalpha():
+#                 stack_6.push(line[i])
+#     for line in strs__: # в начало цифры
+#         for i in range(len(line)):
+#             if line[i].isdigit():
+#                 stack_6.push(line[i])
+#     file.write("\n")
+#     file.write("\n")
+#     file.write("===================================================")
+#     file.write("\n")
+#     file.write("\n")
+#     while not stack_6.is_empty():
+#         el = stack_6.pop()
+#         file.write(el)
+#         file.write("\n")
+
+# -------------------------------------------------------------------------
+# Задание 7
 # Дан файл из целых чисел.
-# Используя дек, за один просмотр файла напечатать сначала все отрицательные числа,
-# затем все положительные числа,
+# Используя дек, за один просмотр файла напечатать
+# сначала все отрицательные числа, затем все положительные числа,
 # сохраняя исходный порядок в каждой группе.
 
-numbers = Deque()
-with open("number_for_7.txt", 'r', encoding='utf-8') as file:
-    nums = file.readlines()
-    for line in nums:
-        num = int(line)
-        if num < 0:
-            numbers.push_bottom(num)
-        if num > 0:
-            numbers.push_top(num)
+# deque_7a = Deque()
+# with open("number_for_7.txt", 'r', encoding="utf-8") as file:
+#     strs = file.readlines()
+#     for line in strs:
+#         num = int(line)
+#         if num < 0:
+#             deque_7a.add_rear(num)
+#     for line in strs:
+#         num = int(line)
+#         if num > 0:
+#             deque_7a.add_rear(num)
+#     while not deque_7a.is_empty():
+#         el = deque_7a.pop_front()
+#         print(el)
 
-    numbers.show_deque()
-
-numbers.show_deque()
-
-# -------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # Задание 8
 
 # Дан текстовый файл.
@@ -70,9 +232,10 @@ numbers.show_deque()
 #     strs = file.readlines()  # прочитаем файл
 #
 # for line in strs:
-#     stack.push(line)  # запишем строки в стек
+#     stack.push(line.strip())  # запишем строки в стек, удаляя лишние пробелы
 #
 # with open("myfile_sorted_8.txt", 'w', encoding="utf-8") as file:  # запишем строки в файл
 #     while not stack.is_empty():
-#         elem = stack.cut()
+#         elem = stack.pop()
 #         file.write(elem)
+#         file.write("\n")
